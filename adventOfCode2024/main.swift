eddc187ee836006020b695a33655229ca3007687
deep60 <deep6060p@gmail.com>
2024-12-15 07:41:11 +0530

    Solve DayOne Ques/PartOne

--- adventOfCode2024/main.swift
+++ adventOfCode2024/main.swift
@@ -1,11 +1,46 @@
-//
-//  main.swift
-//  adventOfCode2024
-//
-//  Created by P Deepanshu on 10/12/24.
-//
 
 import Foundation
 
-print("Hello, World!")
+let filename = "data.txt"
+let currentPath = FileManager.default.currentDirectoryPath
+let filePath = "\(currentPath)/\(filename)"
+
+do {
+    let content = try String(contentsOfFile: filePath, encoding: .utf8)
+    let lines = content.components(separatedBy: .newlines)
+    let dataPoints = lines.filter { !$0.isEmpty }
+    
+    var left: [Int] = []
+    var right: [Int] = []
+    
+    for line in dataPoints {
+        let parts = line.split(separator: " ", omittingEmptySubsequences: true)
+        if parts.count >= 2,
+           let leftValue = Int(parts.first ?? ""),
+           let rightValue = Int(parts.last ?? "") {
+            left.append(leftValue)
+            right.append(rightValue)
+        } else {
+            print("Skipping invalid line: \(line)")
+        }
+    }
+    
+    guard left.count == right.count else {
+        print("Error: Mismatched left and right counts")
+        exit(1)
+    }
+    
+    left.sort()
+    right.sort()
+    
+    var distances: [Int] = []
+    
+    for (index, num) in left.enumerated() {
+        distances.append(abs(num - right[index]))
+    }
+    
+    print(distances.reduce(0, +))
+} catch {
+    print("Error: \(error.localizedDescription)")
+}
 
