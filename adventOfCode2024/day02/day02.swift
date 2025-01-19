a8f739af37da305f607a447ee32ff1ef7ec86974
deep60 <deep6060p@gmail.com>
2025-01-19 11:42:46 +0530

    advent of code day03

--- adventOfCode2024/day02/day02.swift
+++ adventOfCode2024/day02/day02.swift
@@ -1,55 +1,57 @@
 import Foundation
 
-struct UnsafeRepo {
+struct UnsafeReport {
     var report: [Int]
     var i: Int
 }
 
 func day02() {
     let fileName = "day02.txt"
-    let curPath = FileManager.default.currentDirectoryPath
-    let filePath = "\(curPath)/\(fileName)"
-    
+    let currentPath = FileManager.default.currentDirectoryPath
+    let filePath = "\(currentPath)/\(fileName)"
+
     do {
         let content = try String(contentsOfFile: filePath, encoding: .utf8)
         let lines = content.components(separatedBy: .newlines).filter { !$0.isEmpty }
-        
-        let data = lines.map { $0.split(separator: "") }
-        
+
+        let data = lines.map { $0.split(separator: " ") }
+
         var safeCount = 0
-        var unsafeReports: [UnsafeRepo] = []
-        
+
+        var unsafeReports: [UnsafeReport] = []
+
         for report in data {
             let formatted = report.map { Int($0)! }
+
             var prev = -1
             var forward = true
-            
+
             for (index, num) in formatted.enumerated() {
                 if prev != -1 {
                     let diff = abs(num - prev)
-                    
+
                     if diff < 1 || diff > 3 {
-                        unsafeReports.append(UnsafeRepo(report: formatted, i: index))
+                        unsafeReports.append(UnsafeReport(report: formatted, i: index))
                         break
                     }
-                    
+
                     if diff == 0 {
-                        unsafeReports.append(UnsafeRepo(report: formatted, i: index))
+                        unsafeReports.append(UnsafeReport(report: formatted, i: index))
                         break
                     }
-                    
+
                     if index == formatted.count - 1 {
                         prev = -1
                         safeCount += 1
                     } else {
                         if forward {
                             if num > formatted[index + 1] {
-                                unsafeReports.append(UnsafeRepo(report: formatted, i: index))
+                                unsafeReports.append(UnsafeReport(report: formatted, i: index))
                                 break
                             }
                         } else {
                             if num < formatted[index + 1] {
-                                unsafeReports.append(UnsafeRepo(report: formatted, i: index))
+                                unsafeReports.append(UnsafeReport(report: formatted, i: index))
                                 break
                             }
                         }
@@ -61,35 +63,35 @@ func day02() {
                         forward = false
                     }
                 }
-                
+
                 prev = num
             }
         }
-        
+
         print("first safe count: \(safeCount)")
-        
+
         for report in unsafeReports {
             var safe = 0
-            
+
             for (index, _) in report.report.enumerated() {
                 var formatted = report.report
                 formatted.remove(at: index)
-                
+
                 var prev = -1
                 var forward = true
-                
+
                 for (index, num) in formatted.enumerated() {
                     if prev != -1 {
                         let diff = abs(num - prev)
-                        
+
                         if diff < 1 || diff > 3 {
                             break
                         }
-                        
+
                         if diff == 0 {
                             break
                         }
-                        
+
                         if index == formatted.count - 1 {
                             prev = -1
                             safe += 1
@@ -111,15 +113,18 @@ func day02() {
                             forward = false
                         }
                     }
-                    
+
                     prev = num
                 }
             }
-            
+
             if safe > 0 {
                 safeCount += 1
             }
         }
+
+        print("final safe count: \(safeCount)")
+
     } catch {
         print("error: \(error.localizedDescription)")
     }
---
+++ adventOfCode2024/day03/day03.swift
@@ -0,0 +1,8 @@
+//
+//  day03.swift
+//  adventOfCode2024
+//
+//  Created by P Deepanshu on 19/01/25.
+//
+
+import Foundation
